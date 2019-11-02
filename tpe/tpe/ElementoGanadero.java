bc6bdf48fb974c02d549b41079b9f4512a4744bb
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-02 20:10:19 -0300

    estuctura del elemento abstracto - ElementoGanadero - master

--- tpe/tpe/ElementoGanadero.java
+++ tpe/tpe/ElementoGanadero.java
@@ -1,5 +1,23 @@
 package tpe;
 
-public abstract class ElementoGanadero {
+import java.util.List;
 
+public abstract class ElementoGanadero {
+	public abstract int getPeso();
+	
+	public abstract int getCantidad();
+	
+	public abstract int getEdad();
+	
+	public abstract String getCategoria();
+	
+	public abstract List<Animal> bucar(Filtro f);
+	
+	public int getPromPeso() {
+		return getPeso() / getCantidad();
+	}
+	
+	public int getPromEdad() {
+		return getEdad() / getEdad();
+	}
 }
