1e20da48f7d43b9816327d2162a44fbe5207b67a
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-02 20:43:09 -0300

    and/or filtros - AndFiltro OrFiltros - devFiltro

---
+++ tpe/tpe/AndFiltro.java
@@ -0,0 +1,33 @@
+package tpe;
+
+public class AndFiltro implements Filtro {
+	Filtro f1;
+	Filtro f2;
+	
+	public AndFiltro(Filtro f1, Filtro f2) {
+		this.f1 = f1;
+		this.f2 = f2;
+	}
+	
+	@Override
+	public boolean cumple(Animal a) {
+		return f1.cumple(a) && f2.cumple(a);
+	}
+
+	public Filtro getF1() {
+		return f1;
+	}
+
+	public void setF1(Filtro f1) {
+		this.f1 = f1;
+	}
+
+	public Filtro getF2() {
+		return f2;
+	}
+
+	public void setF2(Filtro f2) {
+		this.f2 = f2;
+	}
+	
+}
