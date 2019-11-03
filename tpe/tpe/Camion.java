4ab980fb664e12298cc2dab9139eecf00a5cc25d
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-03 10:55:03 -0300

    se agrega funcionalidad al camion - Camion class - devCamion

--- tpe/tpe/Camion.java
+++ tpe/tpe/Camion.java
@@ -1,8 +1,37 @@
 package tpe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Camion {
 	int capacidad;
 	List<Animal> carga;
+	
+	public Camion(int capacidad) {
+		this.capacidad = capacidad;
+		carga = new ArrayList<>();
+	}
+	
+	public boolean agregar(Animal a) {
+		if(carga.size() < getCapacidad()) {
+			return carga.add(a);
+		}
+		return false;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public List<Animal> getCarga() {
+		return carga;
+	}
+	
+	public boolean isCompleto() {
+		return carga.size() == getCapacidad();
+	}
 }
