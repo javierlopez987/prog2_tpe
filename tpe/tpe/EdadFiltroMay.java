ad6bd66c38b82b72f3393e22ceaa6f5f3af99e98
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-02 21:40:50 -0300

    Filtros demas atributos de Animal - CapadoFiltro EdadFiltroMay EdadFiltroMen SexoFiltro TenerosParidosFiltro - devFiltro

---
+++ tpe/tpe/EdadFiltroMay.java
@@ -0,0 +1,23 @@
+package tpe;
+
+public class EdadFiltroMay implements Filtro {
+	int edad;
+	
+	public EdadFiltroMay(int edadMeses) {
+		edad = edadMeses;
+	}
+
+	@Override
+	public boolean cumple(Animal a) {
+		return a.getEdad() > getEdad();
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+}
