dc6afd9e36e2e63919e58f37b55cd6c9989773af
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-02 23:27:53 -0300

    se incorporan funciones a Animal - Animal class - master

--- tpe/tpe/Animal.java
+++ tpe/tpe/Animal.java
@@ -1,5 +1,7 @@
 package tpe;
 
+import java.util.List;
+
 public class Animal extends ElementoGanadero{
 	int id;
 	int edadMeses;
@@ -9,16 +11,44 @@ public class Animal extends ElementoGanadero{
 	boolean isCapado;
 	int ternerosParidos;
 	
+	public Animal(int i, int e, int p, String r, String s) {
+		id = i;
+		edadMeses = e;
+		peso = p;
+		raza = r;
+		sexo = s;
+		isCapado = false;
+		ternerosParidos = 0;
+	}
+	
+	public void parir(int terneros) {
+		if(terneros > 0) {
+			ternerosParidos += terneros;
+		}
+	}
+	
+	public void capar() {
+		isCapado = true;
+	}
+
+	public void setEdadMeses(int edadMeses) {
+		this.edadMeses = edadMeses;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
 	public String getRaza() {
 		return raza;
 	}
 
 	public int getId() {
 		return id;
-	}
-
-	public int getEdadMeses() {
-		return edadMeses;
 	}
 
 	public int getPeso() {
@@ -29,12 +59,33 @@ public class Animal extends ElementoGanadero{
 		return sexo;
 	}
 
+	public int getTernerosParidos() {
+		return ternerosParidos;
+	}
+	
 	public boolean isCapado() {
 		return isCapado;
 	}
 
-	public int getTenerosParidos() {
-		return ternerosParidos;
+	@Override
+	public int getCantidad() {
+		return 1;
 	}
-	
+
+	@Override
+	public int getEdad() {
+		return edadMeses;
+	}
+
+	@Override
+	public String getCategoria() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Animal> buscar(Filtro f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
